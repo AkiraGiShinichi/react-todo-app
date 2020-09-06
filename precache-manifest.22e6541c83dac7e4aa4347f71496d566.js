ccbde26db6e26a4835e464f602565507c2e87c32
AkiraGiShinichi <akiragishinichi@gmail.com>
2020-09-06 15:19:20 +0800

    Updates

---
+++ precache-manifest.22e6541c83dac7e4aa4347f71496d566.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad447787805a1370836974455d9e6d4",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "be58a2ef5e07583ce99b",
+    "url": "/react-todo-app/static/css/main.cfa66741.chunk.css"
+  },
+  {
+    "revision": "c946ecffc5f0523ff0fc",
+    "url": "/react-todo-app/static/js/2.0c202e56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.0c202e56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be58a2ef5e07583ce99b",
+    "url": "/react-todo-app/static/js/main.d0470d5a.chunk.js"
+  },
+  {
+    "revision": "33cfaab3bd097f416014",
+    "url": "/react-todo-app/static/js/runtime-main.668ca8ac.js"
+  }
+]);
